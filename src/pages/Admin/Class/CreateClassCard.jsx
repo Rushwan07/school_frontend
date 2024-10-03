cb2a0845e9e52ce0fabd6ba7286753248efac879
Subin <46subin@gmail.com>
2024-10-03 21:47:52 +0530

    adding class and delte class are complted

---
+++ src/pages/Admin/Class/CreateClassCard.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { Dialog, DialogContent, DialogFooter, DialogTrigger } from "@/components/ui/dialog";
+import { Pencil, Plus } from "lucide-react";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import Multiselect from "multiselect-react-dropdown";
+// name
+// capacity
+// supervisorId
+// lessons
+const CreateClassCard = () => {
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [selectedSubjects, setSelectedSubjects] = useState([]);
+    const [selectedStaff, setSelectedStaff] = useState("");
+    const staffMembers = [
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Jane Smith" },
+        { id: 3, name: "Michael Johnson" },
+        { id: 4, name: "Emily Davis" },
+    ];
+    const subjects = [
+        { id: 1, name: "Mathematics" },
+        { id: 2, name: "Science" },
+        { id: 3, name: "History" },
+        { id: 4, name: "Geography" },
+        { id: 5, name: "English" },
+        { id: 6, name: "Physical Education" },
+    ];
+
+    const handleSubmit = async () => {
+        setLoading(true);
+        try {
+            console.log(selectedStaff);
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+
+            setDialogOpen(false);
+        } catch (error) {
+            console.error("An error occurred:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    const handleSelectChange = (value) => {
+        setSelectedStaff(value);
+    };
+    return (
+        <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+            <DialogTrigger>
+                <button className="btn btn-sm btn-outline-primary rounded-full flex justify-center items-center gap-1">
+                    <Plus size={18} />
+                </button>
+            </DialogTrigger>
+            <DialogContent className="max-h-[80vh] overflow-x-hidden overflow-y-auto">
+                <div className="space-y-5">
+                    <div>
+                        <Label>Name</Label>
+                        <Input type="text" placeholder="Name" />
+                    </div>
+                    <div>
+                        <Label>Capacity</Label>
+                        <Input type="text" placeholder="Capacity" />
+                    </div>
+                    <div>
+                        <Label>Subjects</Label>
+                        <Multiselect
+                            options={subjects} // Options to display in the dropdown
+                            selectedValues={selectedSubjects} // Preselected value to persist in dropdown
+                            onSelect={(selectedList) => setSelectedSubjects(selectedList)} // Function will trigger on select event
+                            onRemove={(selectedList) => setSelectedSubjects(selectedList)} // Function will trigger on remove event
+                            displayValue="name" // Property to display in the dropdown
+                            className="rounded-lg"
+                        />
+                    </div>
+                    <div>
+                        <label>Staff</label>
+                        <Select onValueChange={handleSelectChange}>
+                            <SelectTrigger className="rounded-lg">
+                                <SelectValue placeholder="Select Staff" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {staffMembers.map((staff) => (
+                                    <SelectItem key={staff.id} value={staff.id.toString()}>
+                                        {staff.name}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </div>
+                <DialogFooter className="sm:justify-end">
+                    <Button onClick={handleSubmit} disabled={loading}>
+                        {loading ? "Saving..." : "Save"}
+                    </Button>
+                </DialogFooter>
+            </DialogContent>{" "}
+        </Dialog>
+    );
+};
+
+export default CreateClassCard;
--- src/pages/Admin/Class/index.jsx
+++ src/pages/Admin/Class/index.jsx
@@ -1,7 +1,131 @@
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { ArrowDownAZ, Pencil, Plus, SlidersHorizontal, Trash } from "lucide-react";
+import React, { useState } from "react";
+
+import CreateClassCard from "./CreateClassCard";
+
+import StudentList from "./EditClassCard";
 
 const Class = () => {
-    return <div>Class</div>;
+    const [classLists, setSlassLists] = useState([
+        {
+            id: 1,
+            class: "Class A",
+            capacity: " 5",
+            supravisor: "Class A",
+            attendanceForToday: true,
+        },
+        {
+            id: 2,
+            class: "Class A",
+            capacity: " 5",
+            supravisor: "Class A",
+            attendanceForToday: false,
+        },
+        {
+            id: 3,
+            class: "Class A",
+            capacity: " 5",
+            supravisor: "Class A",
+            attendanceForToday: true,
+        },
+        {
+            id: 3,
+            class: "Class A",
+            capacity: " 5",
+            supravisor: "Class A",
+            attendanceForToday: false,
+        },
+        {
+            id: 3,
+            class: "Class A",
+            capacity: " 5",
+            supravisor: "Class A",
+            attendanceForToday: false,
+        },
+        {
+            id: 3,
+            class: "Class A",
+            capacity: " 5",
+            supravisor: "Class A",
+            attendanceForToday: false,
+        },
+        {
+            id: 3,
+            class: "Class A",
+            capacity: " 5",
+            supravisor: "Class A",
+            attendanceForToday: false,
+        },
+    ]);
+
+    const columns = [
+        { header: "Class", accessor: "Class" },
+        { header: "Capacity", accessor: "Capacity" },
+        { header: "	Supervisor", accessor: "	Supervisor" },
+
+        { header: "Actions", accessor: "Actions", style: "hidden md:table-cell" },
+    ];
+
+    const renderRow = (item) => (
+        <tr
+            key={item.id}
+            className={`border-b  border-gray-200   rounded  text-sm   ${
+                item?.attendanceForToday && "bg-green-100"
+            }`}
+        >
+            <td className="text-center py-4">{item?.class}</td>
+            <td className="text-center">{item?.capacity}</td>
+            <td className="text-center">{item?.supravisor}</td>
+
+            <td className="flex items-center justify-center h-full py-4 gap-2 text-center">
+                <StudentList classId={item?.class} />
+
+                {/* <button className="btn btn-sm btn-outline-danger rounded-full ml-2 flex justify-center items-center gap-1 ">
+                    <Trash size={18} /> Delete
+                </button> */}
+            </td>
+        </tr>
+    );
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+            <div className="flex items-center justify-between mb-5">
+                <h1 className="text-lg font-semibold hidden md:block">All Classes</h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+                    <Input
+                        type="text"
+                        placeholder="Search classes"
+                        className="border rounded px-3 py-2"
+                    />
+                    <div className="flex items-center gap-4 self-end ">
+                        <button className="w-8 h-8 p-2 flex items-center justify-center rounded-full bg-yellow-400">
+                            <SlidersHorizontal />
+                        </button>
+                        <button className="w-8 h-8 p-2 flex items-center justify-center rounded-full bg-yellow-400">
+                            <ArrowDownAZ />
+                        </button>
+                        <CreateClassCard />
+                    </div>
+                </div>
+            </div>
+            {/* LIST */}
+            <table className="table-auto w-full mx-auto shadow-md rounded">
+                <thead>
+                    <tr className="bg-gray-100 text-center  text-xs font-semibold">
+                        {columns?.map((column) => (
+                            <th
+                                key={column.header}
+                                className={`px-6 py-3 max-w-[200px] ${column.style} `}
+                            >
+                                {column?.header}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>{classLists?.map(renderRow)}</tbody>
+            </table>
+        </div>
+    );
 };
 
 export default Class;
