103df298a4e77c5dc8f8f490c3f4d6803778989a
Rushwan07 <rushdeveloper07@gmail.com>
2024-10-05 19:33:50 +0530

    Parent and student added

--- src/pages/Teachers/Results/index.jsx
+++ src/pages/Teachers/Results/index.jsx
@@ -1,7 +1,158 @@
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { ArrowDownAZ, Plus, SlidersHorizontal } from "lucide-react";
+import React, { useState } from "react";
+import { MdAdd } from "react-icons/md";
+import { Textarea } from "@/components/ui/textarea";
 
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+
+import { Button } from "@/components/ui/button";
 const Results = () => {
-    return <div>Results</div>;
+    const [isOpen, setIsOpen] = useState(false);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("Profile updated locally!");
+        setIsOpen(false); // Close the dialog
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setTeacher((prev) => ({ ...prev, [name]: value }));
+    };
+    return (
+        <div>
+            <div className="flex items-center justify-between mb-5">
+                <h1 className="text-lg font-semibold hidden md:block">Results</h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+                    <Input
+                        type="text"
+                        placeholder="Search Results"
+                        className="border rounded px-3 py-2"
+                    />
+                    <div className="flex items-center gap-4 self-end ">
+                        <button className="w-8 h-8 p-2 flex items-center justify-center rounded-full bg-yellow-400">
+                            <SlidersHorizontal />
+                        </button>
+                        <button className="w-8 h-8 p-2 flex items-center justify-center rounded-full bg-yellow-400">
+                            <ArrowDownAZ />
+                        </button>
+                        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+                            <DialogTrigger asChild>
+                                <div>
+                                    {" "}
+                                    <MdAdd size={30} className="cursor-pointer" />{" "}
+                                </div>
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Add Lesson</DialogTitle>
+                                </DialogHeader>
+                                <form onSubmit={handleSubmit}>
+                                    <Select label="Student" className="mb-3">
+                                        <SelectTrigger className="mb-3">
+                                            <SelectValue placeholder="Select student" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="1">John</SelectItem>
+                                            <SelectItem value="2">Rock</SelectItem>
+                                            <SelectItem value="3">Randy orton</SelectItem>
+                                            <SelectItem value="4">Khalli</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                    <Select className="mb-3">
+                                        <SelectTrigger className="mb-3">
+                                            <SelectValue placeholder="Select Subject" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="MALE">MAths</SelectItem>
+                                            <SelectItem value="2">Science</SelectItem>
+                                            <SelectItem value="3">Physics</SelectItem>
+                                            <SelectItem value="4">Chemistry</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                    <Input
+                                        label="Title"
+                                        name="title"
+                                        onChange={handleChange}
+                                        required
+                                        className="mb-3"
+                                        placeholder="Title"
+                                    />
+                                    <Textarea
+                                        label="Address"
+                                        name="address"
+                                        onChange={handleChange}
+                                        className="mb-3"
+                                        placeholder="Description"
+                                        required
+                                    />
+                                    <Select label="Sex" className="mb-3">
+                                        <SelectTrigger className="mb-3">
+                                            <SelectValue placeholder="Select Subject" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="MALE">MAths</SelectItem>
+                                            <SelectItem value="2">Science</SelectItem>
+                                            <SelectItem value="3">Physics</SelectItem>
+                                            <SelectItem value="4">Chemistry</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+
+                                    <Select label="Sex" className="mb-3">
+                                        <SelectTrigger className="mb-3">
+                                            <SelectValue placeholder="Select Class" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="MALE">Class 9-A</SelectItem>
+                                            <SelectItem value="2">Class 10-A</SelectItem>
+                                            <SelectItem value="3">Class 10-B</SelectItem>
+                                            <SelectItem value="4">Class 10-C</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+
+                                    <Input
+                                        label="Date"
+                                        type="date"
+                                        name="Date"
+                                        onChange={handleChange}
+                                        required
+                                        className="mb-3"
+                                    />
+                                    <Input
+                                        label="Duration"
+                                        type="number"
+                                        name="duration"
+                                        onChange={handleChange}
+                                        required
+                                        placeholder="Duration in minute"
+                                        className="mb-3"
+                                    />
+
+                                    <DialogFooter>
+                                        <Button type="submit">Create</Button>
+                                    </DialogFooter>
+                                </form>
+                            </DialogContent>
+                        </Dialog>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Results;
--- src/pages/Teachers/Students/index.jsx
+++ src/pages/Teachers/Students/index.jsx
@@ -1,7 +1,96 @@
-import React from "react";
-
+import { Input } from "@/components/ui/input";
+import { ArrowDownAZ, Plus, SlidersHorizontal } from "lucide-react";
+import React, { useState } from "react";
 const Student = () => {
-    return <div>Student</div>;
+    const [students, setStudents] = useState([
+
+        {
+            id: 1,
+            name: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+            reg: "0217",
+            // description: " This is an important update for Class AThis is  ",
+            class: "Test@gmail.com",
+            gender: "Male",
+            parent: "Lorem ipsum dolor sit amet consectetur.",
+            phone: "+91-9999999999",
+
+        },
+    ]);
+
+    const columns = [
+        { header: "Name", accessor: "name", style: "hidden md:table-cell" },
+        { header: "RegNo", accessor: "reg" },
+        { header: "Class", accessor: "class" },
+        { header: "Gender", accessor: "gender" },
+        { header: "Parent Name", accessor: "parent", style: "hidden md:table-cell" },
+        { header: "Parent Phone", accessor: "phone", },
+        // { header: "Actions", accessor: "Actions", style: "hidden md:table-cell" },
+    ];
+
+    const renderRow = (item) => (
+        <tr
+            key={item.id}
+            className="border-b border-gray-200 bg-white shadow-md rounded even:bg-slate-50 text-sm hover:bg-gray-100"
+        >
+            <td className="flex items-center gap-4 py-4 px-6">
+                <div>
+                    <p className="font-semibold">{item?.name}</p>
+                    {/* <p className="text-xs text-gray-500">{item?.name}</p> */}
+                </div>
+            </td>
+            <td className="text-center">{item?.reg}</td>
+            <td className="hidden md:table-cell text-center">{item?.class}</td>
+            <td className="hidden md:table-cell text-center ">{item?.gender}</td>
+            <td className="hidden md:table-cell text-center w-[400px] ">{item?.name}</td>
+            <td className="hidden md:table-cell text-center ">{item?.phone}</td>
+            {/* <td className="flex items-center justify-center gap-2 text-center">
+                <button className="btn btn-sm btn-outline-primary rounded-full ">
+                    <i className="fa fa-edit" aria-hidden="true"></i> Edit
+                </button>
+                <button className="btn btn-sm btn-outline-danger rounded-full ml-2 ">
+                    <i className="fa fa-trash" aria-hidden="true"></i> Delete
+                </button>
+            </td> */}
+        </tr>
+    );
+    return (
+        <div>
+            {" "}
+            <div className="flex items-center justify-between mb-5">
+                <h1 className="text-lg font-semibold hidden md:block">Students</h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+                    <Input
+                        type="text"
+                        placeholder="Search by student roll No"
+                        className="border rounded px-3 py-2"
+                    />
+                    <div className="flex items-center gap-4 self-end ">
+                        <button className="w-8 h-8 p-2 flex items-center justify-center rounded-full bg-yellow-400">
+                            <SlidersHorizontal />
+                        </button>
+                        <button className="w-8 h-8 p-2 flex items-center justify-center rounded-full bg-yellow-400">
+                            <ArrowDownAZ />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <table className="table-auto w-full mx-auto shadow-md rounded">
+                <thead>
+                    <tr className="bg-gray-100 text-center  text-xs font-semibold">
+                        {columns?.map((column) => (
+                            <th
+                                key={column.header}
+                                className={`px-6 py-3 max-w-[200px] ${column.style} `}
+                            >
+                                {column?.header}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>{students?.map(renderRow)}</tbody>
+            </table>
+        </div>
+    );
 };
 
 export default Student;
