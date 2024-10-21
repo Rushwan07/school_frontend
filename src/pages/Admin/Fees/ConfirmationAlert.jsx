930715a0f1506390845b3aed01482623058473f0
Subin <46subin@gmail.com>
2024-10-21 21:18:31 +0530

    admin fees paymnt added

--- src/pages/Admin/Fees/ConfirmationAlert.jsx
+++ src/pages/Admin/Fees/ConfirmationAlert.jsx
@@ -11,18 +11,52 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import axios from "axios";
+import { toast } from "@/hooks/use-toast";
 
-const ConfirmationAlert = ({ name, regNo, fees, className }) => {
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const ConfirmationAlert = ({
+    name,
+    regNo,
+    basefees,
+    totalfees,
+    transportationFees,
+    className,
+    feesId,
+    setStudents,
+    token,
+}) => {
     const [loading, setLoading] = useState(false);
 
-    const handleConfirm = () => {
+    const handleConfirm = async () => {
         //studentId, regNo,baseFees, transportationFees, totalFees
         try {
             setLoading(true);
+            const res = await axios.patch(
+                BASE_URL + "/fees/update-status/" + feesId,
+                {},
+                {
+                    headers: { token: token },
+                },
+            );
+            console.log(res);
 
+            setStudents((prevStudents) => {
+                console.log("Previous Students:", prevStudents); // Check previous students
+                return prevStudents?.map((student) => {
+                    console.log("Checking student:", student); // Log each student's regno
+                    if (student?.studentId.regno == regNo) {
+                        console.log("Updating student:", student.regno); // Log when a student is updated
+                        return { ...student, isPaid: true }; // Update the isPaid status
+                    }
+                    return student; // Return the original student if no match
+                });
+            });
             setLoading(false);
         } catch (error) {
             console.log(error);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -47,8 +81,16 @@ const ConfirmationAlert = ({ name, regNo, fees, className }) => {
                                 <strong>Class:</strong> {className}
                             </p>
                             <p>
-                                <strong>Fees:</strong>{" "}
-                                {fees?.map((fee) => `${fee.name}: (${fee.fee})`).join(", ")}
+                                <strong>Base Fees:</strong>
+                                {basefees || "--"}
+                            </p>
+                            <p>
+                                <strong>Transportation Fees:</strong>
+                                {transportationFees || "--"}
+                            </p>
+                            <p>
+                                <strong>Total Fees:</strong>
+                                {totalfees || "--"}
                             </p>
                         </div>
                         This action cannot be undone.
